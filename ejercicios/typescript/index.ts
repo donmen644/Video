3c266484cdc70a363bd802d8c92c44ca7f42bcd1
donmen644 <darksiguapo.1@gmail.com>
2021-04-01 00:50:44 -0600

    prueba 1:

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -1,40 +1,85 @@
-// Boolean
-let muted: boolean = true;
-muted = false;
+// // Boolean
+// let muted: boolean = true;
+// muted = false;
 
-//Numeros
-let age = 6;
-let numerador: number = 42;
-let denominador: number = age;
-let resultado = numerador / denominador;
+// //Numeros
+// let age = 6;
+// let numerador: number = 42;
+// let denominador: number = age;
+// let resultado = numerador / denominador;
 
-//string
-let nombre: string = 'Richard';
-let saludo =`Me llamo ${nombre}`;
+// //string
+// let nombre: string = 'Richard';
+// let saludo =`Me llamo ${nombre}`;
 
-//Arreglos
-let people: string[] = [];
-people = ['Isabel', 'Nicole', 'Raul'];
-//people.push("9000")
+// //Arreglos
+// let people: string[] = [];
+// people = ['Isabel', 'Nicole', 'Raul'];
+// //people.push("9000")
 
-let peopleAndNumbers: Array<string | number> = [];
-peopleAndNumbers.push('Ricardo');
-peopleAndNumbers.push(9001);
+// let peopleAndNumbers: Array<string | number> = [];
+// peopleAndNumbers.push('Ricardo');
+// peopleAndNumbers.push(9001);
 
-//Enum
-enum Color{
+// //Enum
+// enum Color{
+//     Rojo = 'Rojo',
+//     Verde = 'Verde',
+//     Azul = 'Azul',
+//     Amarillo = 'Amarillo',
+// }
+
+// let colorFavorito: Color = Color.Amarillo;
+// console.log(`Mi color favorito es ${colorFavorito}`);
+
+// //Any
+// let comodin: any = 'Joker';
+// comodin = {type: 'Wildcard'};
+
+// //Object
+// let someObjet: object = {type: 'Wildcard'};
+
+//Funciones 
+// function add(a: number, b: number): number{
+//     return a + b;
+// }
+//     const sum = add(4, 6);
+
+//     function createAdder(a: number): (number) => number {
+//         return function(b: number){
+//            return b + a;
+//         };
+//     }
+//  const richard = fullName('agente');
+//  console.log(richard);
+
+//Interfaces
+enum Color {
     Rojo = 'Rojo',
     Verde = 'Verde',
-    Azul = 'Azul',
-    Amarillo = 'Amarillo',
 }
 
-let colorFavorito: Color = Color.Amarillo;
-console.log(`Mi color favorito es ${colorFavorito}`);
+interface Rectangulo {
+    ancho: number;
+    alto: number;
+    color?: Color;
+}
 
-//Any
-let comodin: any = 'Joker';
-comodin = {type: 'Wildcard'};
+let rect: Rectangulo = {
+    ancho: 4,
+    alto: 6,
+    //color: Color.Rojo,
+};
 
-//Object
-let someObjet: object = {type: 'Wildcard'};
+function area(r: Rectangulo): number{
+    return r.alto * r.ancho;
+}
+
+const areaRect = area(rect);
+console.log(areaRect);
+
+rect.toString = function(){
+    return this.color ? `Un Rectangulo ${this.color}`: `Un Rectangulo`;
+};
+
+console.log(rect.toString());
